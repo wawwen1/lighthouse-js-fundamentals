122179ccde57fc22bcc56dc057768a8ec7d9dea7
warren roque <warrenroque17@gmail.com>
2022-02-22 18:36:29 +0000

    Saying hello without repeating console

---
+++ hello.js
@@ -0,0 +1,13 @@
+//ex1
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+//ex2
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Warren");

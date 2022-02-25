cf0d7348841afddcdb7fc5eea388192b86a20f12
warren roque <warrenroque17@gmail.com>
2022-02-25 01:00:17 +0000

    creating arrays using loopswith a range

---
+++ looping-range.js
@@ -0,0 +1,20 @@
+let range = function (start, end, step) {
+let finalArray = [];
+
+  if (step <= 0) {
+    return finalArray;
+  } else if (start > end) {
+    return finalArray;
+  } else if (start === false || end === false || step === false) {
+    return finalArray;
+  }
+  
+  for (let i = start; i <= end; i += step) {
+    finalArray.push(i);
+  }
+  return finalArray;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
